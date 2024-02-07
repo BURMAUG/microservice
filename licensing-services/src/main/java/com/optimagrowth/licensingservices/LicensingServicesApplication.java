1720c8cd66af028ee789471031b9868d91502b84
Burmau Garba <burmaud1@gmail.com>
2024-02-06 19:26:16 -0500

    init

---
+++ licensing-services/src/main/java/com/optimagrowth/licensingservices/LicensingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicensingServicesApplication.class, args);
+    }
+
+}
---
+++ licensing-services/src/test/java/com/optimagrowth/licensingservices/LicensingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.licensingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
